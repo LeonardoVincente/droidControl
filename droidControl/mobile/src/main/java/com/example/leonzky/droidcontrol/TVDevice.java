dbedd8ff1e281c39d3c4ce281ec95428bdd5e4fe
leonzky <leonzkyhv@gmail.com>
2015-06-07 16:43:51 -0700

    First Meesage exchange
    Send messages between the TV and the Android app

--- droidControl/mobile/src/main/java/com/example/leonzky/droidcontrol/TVDevice.java
+++ droidControl/mobile/src/main/java/com/example/leonzky/droidcontrol/TVDevice.java
@@ -1,6 +1,13 @@
 package com.example.leonzky.droidcontrol;
-import com.samsung.multiscreen.device.Device;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+
 import com.samsung.multiscreen.application.Application;
+import com.samsung.multiscreen.device.Device;
 /**
  * Created by leonzky on 6/7/2015.
  */
@@ -22,6 +29,29 @@ public class TVDevice {
             INSTANCE = new TVDevice();
         }
         return INSTANCE;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+
+    public String getUserName(Context context) {
+        // Get Default Device Name
+        String name = android.os.Build.MODEL;
+
+        // Look for account name if exists
+        AccountManager accountManager = AccountManager.get(context);
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        if (accounts.length > 0) {
+            Account account = accounts[0];
+            name = account.name;
+        }
+        return name;
+    }
+    public void runOnUIThread(Runnable runnable) {
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        mainHandler.post(runnable);
     }
 
 }
--- droidControl/mobile/src/main/java/com/example/leonzky/droidcontrol/mainControl.java
+++ droidControl/mobile/src/main/java/com/example/leonzky/droidcontrol/mainControl.java
@@ -2,15 +2,29 @@ package com.example.leonzky.droidcontrol;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class mainControl extends Activity {
+import com.samsung.multiscreen.channel.Channel;
+import com.samsung.multiscreen.channel.ChannelClient;
+import com.samsung.multiscreen.device.Device;
+import com.samsung.multiscreen.device.DeviceAsyncResult;
+import com.samsung.multiscreen.device.DeviceError;
+import com.samsung.multiscreen.channel.IChannelListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class mainControl extends Activity  implements IChannelListener{
+    private static final String LOGTAG = "mainControl";
+    private Channel channel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_control);
+        connectToChannel();
     }
 
     @Override
@@ -33,5 +47,96 @@ public class mainControl extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onResume() {
+        Log.d(LOGTAG, "onResume()");
+        connectToChannel();
+        super.onResume();
+    }
+    private void connectToChannel() {
+        final Device device = TVDevice.getInstance().getDevice();
+        if( device == null ){
+            Toast toast = Toast.makeText(getApplicationContext(), "No device selected", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+            Log.d(LOGTAG,"error device null");
+
+            return ;
+        }
+        disconnectChannel();
+        final String channelId = TVDevice.getInstance().getChannelId();
+        final String userName = TVDevice.getInstance().getUserName(this);
+        Log.d(LOGTAG, "connectToChannel() userName: " + userName + " channelID " + channelId);
+
+        Map<String, String> clientAttributes = new HashMap<String, String>();
+        clientAttributes.put("name", userName);
+
+        device.connectToChannel(channelId, clientAttributes, new DeviceAsyncResult<Channel>() {
+
+            public void onResult(final Channel channel) {
+                TVDevice.getInstance().runOnUIThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Connected to device channel", Toast.LENGTH_SHORT);
+                        toast.show();
+                        Log.d(LOGTAG, "SUCEESS");
+                        mainControl.this.channel = channel;
+                        channel.setListener(mainControl.this);
+                        channel.sendToAll("Send to all");
+                    }
+                });
+
+            }
+            public void onError(final DeviceError error) {
+
+                TVDevice.getInstance().runOnUIThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast toast = Toast.makeText(getApplicationContext(), "FAIL!!!!! connect to device channel", Toast.LENGTH_SHORT);
+                        toast.show();
+                        Log.d(LOGTAG, "Fail");
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void disconnectChannel() {
+        if (channel != null) {
+            channel.setListener(null);
+            channel.disconnect();
+            channel = null;
+        }
+    }
+
+    @Override
+    public void onConnect() {
+        Log.d(LOGTAG, "Connected to channel");
+        //channel.broadcast("Hello my friends");
+        channel.sendToAll("Even ME");
+    }
+
+    @Override
+    public void onDisconnect() {
+
+    }
+
+    @Override
+    public void onClientConnected(ChannelClient channelClient) {
+        Log.d(LOGTAG, "onClienConected");
+        //channel.broadcast("Hello my friends");
+        channel.sendToAll("On client conected Even ME");
+    }
+
+    @Override
+    public void onClientDisconnected(ChannelClient channelClient) {
+
+    }
+
+    @Override
+    public void onClientMessage(ChannelClient channelClient, String s) {
+        Log.d(LOGTAG, "onClientMessage() " + s);
     }
 }
